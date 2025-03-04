d5ff807f8516ee2c358bdfeb4ed8972c750268bf
Kunal Singh <kunal_singh@outlook.com>
2025-03-04 21:53:08 +0530

    learned how to access camera using cv2

---
+++ 1.py
@@ -0,0 +1,24 @@
+import cv2
+
+cap = cv2.VideoCapture(0)  #Access camera(but doesn't display it)
+if not cap.isOpened:
+    print("Error can't access camera.")
+
+while True:
+    ret, frame= cap.read()
+    #ret= a boolean variable, True if frame is captured, False if frams isn't captured
+    #frame= image from the camera 
+    #cap.read()= returns the value of ret and frame
+    if not ret:
+        print("Failed to display frame.")
+        break
+
+    cv2.imshow("webcam feed", frame)  #cv2.imshow = displays the image from camera
+    
+    if cv2.waitKey(1)== ord('q'):
+    #.waitKey(1)= waits for 1 millisecond and returns the ASCII value if any key is pressed, and if no key is pressed it returns -1(meaning no key was detected).
+    #ord('q') = ASCII value of q is 113 and ord('q') has the same value.(by pressing q you close the cv2 camera window).
+        break
+
+cap.release()  # camera access is revoked 
+cv2.destroyAllWindows()   # closes any cv2 window 
