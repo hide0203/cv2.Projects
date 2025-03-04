80ea7d2d104139a6f4ed4f451149d083a6a33e40
Kunal Singh <kunal_singh@outlook.com>
2025-03-05 04:31:24 +0530

    wip

--- 3.py
+++ 3.py
@@ -1,4 +1,12 @@
 import cv2
 
+cap = cv2.VideoCapture()
+
+ret, frame = cap.read()
+if not ret:
+    print("can't grab frame")
+
+
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 print(cv2.data.haarcascades)
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
