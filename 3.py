76cbc8d4d58c9083658378cd7802a8a6af54248c
Kunal Singh <kunal_singh@outlook.in>
2025-03-08 21:14:26 +0530

    completed
    detects face and draws a rectangle around detected face

--- 3.py
+++ 3.py
@@ -1,12 +1,26 @@
 import cv2
 
-cap = cv2.VideoCapture()
-
-ret, frame = cap.read()
-if not ret:
-    print("can't grab frame")
-
-
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-print(cv2.data.haarcascades)
-gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("can't grab frame")
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)  #Detect faces
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2) #Draw rectangle around the faces
+
+    cv2.imshow("Face Detection", frame) #Display the frame
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release resources
+cap.release()
+cv2.destroyAllWindows()
